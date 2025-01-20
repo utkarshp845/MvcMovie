878342b8fd0551e3dbfdaf6ae1a2b1a922292937
Utkarsh Pandey <utkarshp845@gmail.com>
2025-01-20 16:59:58 +0000

    Some UI updates

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0823e74a0b65f66abd77b8fb5802a02b4646a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e04c26c9a15fa7278d4ecfc1c9f7a4c2d662675")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
